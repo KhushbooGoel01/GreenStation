aaf750f9906f3b779b3361d386cdb82b8eabcbbe
Khushboo Goel <Khushu.goel8@gmail.com>
2022-04-11 03:33:07 +0530

    first commit

---
+++ src/components/ImageCard.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { Collapse } from '@material-ui/core';
+import PlaceToVisit from './PlaceToVisit';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 645,
+    background: 'rgba(0,0,0,0.5)',
+    margin: '20px',
+  },
+  media: {
+    height: 440,
+  },
+  title: {
+    fontFamily: 'Nunito',
+    fontWeight: 'bold',
+    fontSize: '2rem',
+    color: '#fff',
+  },
+  desc: {
+    fontFamily: 'Nunito',
+    fontSize: '1.1rem',
+    color: '#ddd',
+  },
+  link:{
+    textDecoration: 'none',
+  }
+});
+
+export default function ImageCard({ place, checked }) {
+  const classes = useStyles();
+
+  return (
+    <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})}>
+       <a href= {place.Url} className={classes.link} > <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image={place.imageUrl}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h1"
+            className={classes.title}
+          >
+          {place.title}
+          </Typography>
+          <Typography
+
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            className={classes.desc}
+          >
+            {place.description}
+          </Typography>
+        </CardContent>
+      </Card>
+      </a></Collapse>
+  );
+}
---
+++ src/static/places.js
@@ -0,0 +1,20 @@
+const places = [
+  {
+    title: 'Apply for Green Station Installment',
+    description:
+      "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/cy4.jpg',
+    Url: 'https://forms.gle/DevA2wka3aSkRCiH6',
+    time: 1500,
+  },
+  {
+    title: 'Find Nearest Green Station',
+    description:
+      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/cy6.png',
+    Url: 'https://www.google.com/maps?q=petrol+pumps+near+me' ,
+    time: 1500,
+  },
+];
+
+export default places;
