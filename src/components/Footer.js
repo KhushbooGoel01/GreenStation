55199938281a7b02897ffbb6526f9ac2da7881c6
Khushboo Goel <Khushu.goel8@gmail.com>
2022-04-11 04:33:22 +0530

    ae

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 // import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
 // import SortIcon from '@material-ui/icons/Sort';
